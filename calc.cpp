449435a8e3f1479da1c4f2e4eaf0c57afd1706c6
Sagarikashahane <Sagarika.Shahane@cumminscollege.in>
2014-07-28 11:09:33 +0530

    Update calc.cpp
    Added more functions

--- calc.cpp
+++ calc.cpp
@@ -2,7 +2,15 @@
 #include<iostream>
 void main()
 {
-int a=10,b=20;
-int c=a+b;
+int a=10,b=20;   //hardcode values
+int c=a+b;    //Addition of two numbers :)
+cout<<c;
+
+ a=10,b=20;   //hardcode values
+ c=a-b;    //subtraction of two numbers :)
+cout<<c;
+
+ a=10,b=20;   //hardcode values
+ c=a*b;    //multiplication of two numbers :)
 cout<<c;
 }
